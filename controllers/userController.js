6b9135a20b703523e02a09fd7917f2dd894c8a6f
urie-w <urieasia@gmail.com>
2024-09-17 10:27:38 -0600

    Thought model done, user controllers started

---
+++ controllers/userController.js
@@ -0,0 +1,59 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+// Get all users
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // Gets single user by id
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .populate('thoughts')
+        .populate('friends')
+        .then((user) => 
+        !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json(user)
+)
+.catch((err) => res.status(500).json(err));
+    },
+
+    // Update single user by id
+updateUser(req,res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+    )
+    .then((user) => 
+    !user
+        ? res.status(404).json({ message: 'No user with this id!' })
+        : res.json(user)
+)
+.catch((err) => res.status(500).json(err));
+},
+
+// Creates new user
+createUser(res,res) {
+    User.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+},
+
+// Deletes user by id
+deleteUser(req,res) { 
+    User.findOneAndDelete({ _id: req.params.userId })
+    .then((user) => 
+    !user
+        ? res.status(404).json({ message: 'No user with that ID' })
+        : Thought.deleteMany({ _id: { $in: user.thoughts } })
+)
+.then(() => res.json({ message: 'User and associated thoughts deleted!' }))
+.catch((err) => res.status(500).json(err) );
+},
+
+
+}
